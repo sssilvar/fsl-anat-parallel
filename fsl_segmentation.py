0c35f2b306acd4eff0e4c9a6f3d62f942fd105cc
Santiago <sssilvar@unal.edu.co>
2019-07-05 15:38:59 -0500

    Initial version

---
+++ fsl_segmentation.py
@@ -0,0 +1,7 @@
+#|/bin/env python
+import os
+from multiprocessing.pool import Pool
+
+if __name__ == '__main__':
+    dataset_folder ='/home/nicolasmg/Paper2018/ABIDE_II_Muestra/' # Hombres_Autistas / Hombres_Controles
+
---
+++ preprocessing_pipeline_b.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+IMAGE=$1  # Image to be processed
+SUBJECT_FOLDER=$2
+MNI_TEMPLATE="/usr/local/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz"  # MNI152 brain template
+
+# Usage message
+USAGE="USAGE:\n\t- preprocessing_pipeline_b.sh [IMAGE_TO_BE_PROCESSED]  [SUBJECT_FOLDER]"
+if [[ ! ${IMAGE} ]]; then
+    echo "[  ERROR  ] Image to be processed not defined."
+    echo -e ${USAGE}
+    exit 1
+fi
+
+if [[ ! ${SUBJECT_FOLDER} ]]; then
+    echo "[  ERROR  ] Subject folder is not defined."
+    echo -e ${USAGE}
+    exit 1
+fi
+
+# =============================================
+# START PIPELINE
+# =============================================
+# Up to bias correction and BET
+eval "fsl_anat -i ${IMAGE} -o ${SUBJECT_FOLDER} --clobber --noseg"
+STRIPPED_BRAIN="${SUBJECT_FOLDER}.anat/T1_biascorr_brain.nii.gz"
+
+# Intensity normalization
+INM_BRAIN="${SUBJECT_FOLDER}/mprage_Correc.nii.gz"  # Intensity normalized volume
+eval "fslmaths ${STRIPPED_BRAIN} -inm 60 ${INM_BRAIN}"
+
+# Rigid egistration
+REGISTERED="${SUBJECT_FOLDER}/Cer_Registrado.nii.gz"
+TRANSF_MAT="${SUBJECT_FOLDER}/Cer_Registrado.mat"
+eval "flirt -in ${MNI_TEMPLATE} -ref ${INM_BRAIN} -out ${REGISTERED} -omat ${TRANSF_MAT} -dof 12"
+
+# Elastic registration
+REGISTERED="${SUBJECT_FOLDER}/Cer_Registrado_Elastico.nii.gz"
+TRANSF_MAT="${SUBJECT_FOLDER}/Cer_Registrado_Elastico.mat"
+WARP_COUT="${SUBJECT_FOLDER}/Elastic_Cout.nii.gz"
+eval "fnirt --ref=${INM_BRAIN} --in=${MNI_TEMPLATE} --iout=${REGISTERED} --aff=${TRANSF_MAT} --cout=${WARP_COUT} --splineorder=2 --imprefm=0 --impinm=0"
+
+# Segmentation
+CORTICAL_ATLAS="/usr/local/fsl/data/atlases/HarvardOxford/HarvardOxford-cort-maxprob-thr25-1mm.nii.gz"
+SUBCORTICAL_ATLAS="/usr/local/fsl/data/atlases/HarvardOxford/HarvardOxford-sub-maxprob-thr25-1mm.nii.gz"
+
+CORTICAL_PARCELLATION="${SUBJECT_FOLDER}/Elastic_Cortical.nii.gz"
+SUBCORTICAL_PARCELLATION="${SUBJECT_FOLDER}/Elastic_SubCortical.nii.gz"
+
+eval "applywarp --ref=${INM_BRAIN} --in=${CORTICAL_ATLAS} --warp=${WARP_COUT} --out=${CORTICAL_PARCELLATION} --interp=nn"
+eval "applywarp --ref=${INM_BRAIN} --in=${SUBCORTICAL_ATLAS} --warp=${WARP_COUT} --out=${SUBCORTICAL_PARCELLATION} --interp=nn"
+
+# Done!
+echo "Done!"
