54afa5693b1851e8978014b3d3a0bcc46fb86983
Santiago <sssilvar@unal.edu.co>
2019-07-05 17:52:50 -0500

    Initial version

--- fsl_segmentation.py
+++ fsl_segmentation.py
@@ -23,7 +23,7 @@ def process_subject(row):
     pipeline = join(root, 'preprocessing_pipeline_b.sh')
     command = f'{pipeline} {in_file} {subject_folder}'
     print(command)
-    os.system('export FSLDIR="/usr/local/fsl"')
+    os.system('FSLDIR="/usr/local/fsl" && PATH=${FSLDIR}/bin:${PATH}')
     os.system(command)
 
 
